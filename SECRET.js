f86258659fd38c5b0a26753bb3ad660b3323a989
ME <hell.gmail.com>
2023-11-05 10:53:26 +0400

    add

--- SECRET.js
+++ SECRET.js
@@ -60,20 +60,37 @@ let formel = [
     el('h2', BODY, ['class', 'headfrm']),
     el('input', BODY, ['id', 'inputnme']),
     el('select', BODY, ['id', 'optionss']),
-    el('button', BODY, ['class', 'submitted'])
+    el('button', BODY, ['class', 'submitted']),
+    el('button', BODY, ['', ''])
 ]
+formel[4].textContent = 'clear'
+formel[4].onclick = () => {
+    localStorage.clear()
+}
 classes.forEach(x => {
     x.forEach(y => {
       el('option', formel[2], ['value', y]).textContent = y
     })
 })
-formel[1].setAttribute('placeholder', 'enter a name')
-formel[0].textContent = 'enter a name and select the class'
-formel[3].textContent = 'enter'
-formel[3].onclick = () => {
+
+function addStuff() {
     let valname = formel[1].value;
     let classnammee = formel[2].value
     let newarr = [valname, classnammee]
     mainObj.push(newarr)
     console.log(mainObj)
+    localStorage.setItem('studts', JSON.stringify(mainObj))
 }
+
+formel[1].setAttribute('placeholder', 'enter a name')
+formel[0].textContent = 'enter a name and select the class'
+formel[3].textContent = 'enter'
+formel[3].onclick = () => {
+    addStuff()
+}
+window.addEventListener('keypress', (e) => {
+    if(e.key == 'n') {
+        addStuff()
+    }
+})
+console.log(mainObj)
